d7f6c30e4e8a9d7e8a4b8edf1b708320cbb99560
Squashim <konrad18v1@wp.pl>
2022-08-17 15:06:22 +0200

    10% done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className='fixed w-full h-20 shadow-xl z-10 bg-white'>
+			<div className='flex justify-between items-center w-full h-full px-4 2xl:px-16'>
+				<Image src='/../assets/logo.svg' width={80} height={80} />
+				<div className='mr-auto ml-3 border-l-2 h-[40%] flex items-center border-black/70'>
+					<p className='text-black uppercase text-xs font-bold px-2'>
+						Konrad Piekarz
+					</p>
+				</div>
+				<div>
+					<ul className='hidden md:flex'>
+						<Link href='/'>
+							<li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+						</Link>
+						<Link href='/'>
+							<li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+						</Link>
+						<Link href='/'>
+							<li className='ml-10 text-sm uppercase hover:border-b'>
+								Projects
+							</li>
+						</Link>
+						<Link href='/'>
+							<li className='ml-10 text-sm uppercase hover:border-b'>
+								Contact
+							</li>
+						</Link>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
